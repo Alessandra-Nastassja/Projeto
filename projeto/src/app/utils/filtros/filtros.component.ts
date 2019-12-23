8eba8fa671cc1e65b4969c14107395a63a70fcb4
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2019-12-22 22:44:10 -0300

    Ajustes no componente

---
+++ projeto/src/app/utils/filtros/filtros.component.ts
@@ -0,0 +1,50 @@
+import { FiltrosService } from "./filtros.service";
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filtros',
+  templateUrl: './filtros.component.html',
+  styleUrls: ['./filtros.component.scss'],
+  providers: [FiltrosService]
+})
+export class FiltrosComponent implements OnInit {
+
+  comidas = []
+  categorias = []
+  idCategoria: number
+
+  constructor(private filtrosService: FiltrosService) {
+    
+  }
+
+  ngOnInit() {
+    this.getCategories()
+  }
+
+  getCategories(){
+    this.filtrosService.getCategory().subscribe(
+      success => {
+        this.categorias = success
+
+        // console.warn(this.categorias);
+      }
+    )
+  }
+
+  getIdProduto(event){
+    this.idCategoria = event.target.id
+    
+    this.getFilterCategory(this.idCategoria)
+  }
+
+  getFilterCategory(event){
+    this.filtrosService.getFilterCategories(event).subscribe(
+      success => {
+        this.comidas = success
+
+        console.warn(this.comidas);
+      }
+    )
+  }
+
+}
---
+++ projeto/src/app/utils/filtros/filtros.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Http } from "@angular/http";
+
+import  "rxjs/add/operator/map";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FiltrosService {
+
+  urlCors = 'https://cors-anywhere.herokuapp.com/';
+
+  idCategoria: number
+
+  constructor(private http: Http) { }
+
+  // Carrega todas as categorias 
+  getCategory(){
+    return this.http.get(this.urlCors + "http://taco-food-api.herokuapp.com/api/v1/category")
+    .map(res => res.json()); 
+  }
+
+  getFilterCategories(idCategoria){
+    return this.http.get(this.urlCors + "https://taco-food-api.herokuapp.com/api/v1/category/"+ idCategoria + "/food")
+    .map(res => res.json());
+  }
+}
